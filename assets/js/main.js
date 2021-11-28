e9da0b558898ea8bf0c5b8af2d4b7b92719c8cb8
the-name-romel <rommelbon100@gmail.com>
2021-11-28 12:35:56 +0800

    Init

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+
+/*==================== ACCORDION SKILLS ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
